6dc93f36f9d627cbf30b34d434a4221d6524bc70
joel gamez <joelgamez14six@yahoo.com>
2018-11-18 21:34:56 -0500

    typos fixed

--- src/components/questionTwo.js
+++ src/components/questionTwo.js
@@ -17,11 +17,11 @@ class QuestionTwo extends Component {
             <h5 Style={'text-align:left;'}>Title: How to purchase socks through Amazon.com</h5>
             <span>
                 <strong>Description: </strong>
-                With an established Internet access, this is how a user can use an Internet browser to purchase socks on Amazon.com.
+                With an established Internet access this is how a user can use an Internet browser to purchase socks on Amazon.com.
             </span>
             <ol>
                 <li>
-                    Once Internet connection has been established, open an Internet browser, such as Google Chrome, Mozilla-Firefox, Safari, or Internet Explorer.
+                    Once Internet connection has been established open an Internet browser such as Google Chrome, Mozilla-Firefox, Safari, or Internet Explorer.
                 
                 </li>
                 <li>
@@ -36,13 +36,13 @@ class QuestionTwo extends Component {
                     
                 </li>
                 <li>
-                    Each result item will be displayed with a graphic presentation of the product, price, and review stars (the more stars filled in yellow, the better the average customer reviews are).
+                    Each result item will be displayed with a graphic presentation of the product, price, and review stars (the more stars filled in yellow the better the average customer reviews are).
                 </li>
                 <li>
                     As you scroll through the results, select an item that you are interested in purchasing. For this example, we will use this specific sock result, http://a.co/d/1mlFefj. Click on this link to inspect the sock item.
                 </li>
                 <li>
-                    To the left of the item show page you will notice an image gallery for the item, and may click on different images to view them larger. On the right you may notice descriptions referring to material, design, and purchase information.
+                    To the left of the item show page you will notice an image gallery for the item; click on different images to view them larger. On the right you may notice descriptions referring to material, design, and purchase information.
                 </li>
                 <li>
                     There will be a value box named “Qty”  meaning quantity. You may click on the box and select the number of orders of this product you desire. 
@@ -74,7 +74,7 @@ class QuestionTwo extends Component {
                     If them item is available, click on the “Add to Cart”  link. Your shopping cart is a collection of items that you want to add to your next order. When you click “Add to Cart”, the item is added to your collection of items in your potential order, and the page redirect to a new page verifying your cart addition. You will see a cart subtotal, and a link to view your cart or proceed to checkout. Click on “Proceed to checkout.”
                 </li>
                 <li>
-                    The new page will prompt you to sign-in. But as you do not have an account, you must create an Amazon account. Click on the “Create your Amazon Account” link. You will register with input into the following fields.
+                    The new page will prompt you to sign-in, but as you do not have an account you must create an Amazon account. Click on the “Create your Amazon Account” link. You will register with input into the following fields.
                     <ul>
                         <li>
                            name
@@ -132,16 +132,16 @@ class QuestionTwo extends Component {
                         <li>Amazon Store Card application</li>
                         <li>Bank Account Addition</li>
                     </ul>
-                    Click “Continue” after filling out the payment information fields, and then you will be redirected to a new page requesting billing information.
+                    Click “Continue” after filling out the payment information fields; you then will be redirected to a new page requesting billing information.
                 </li>
                 <li>
-                    Click on “Use this Address” to make your shipping & billing information identical, or enter a different billing address with the provided fields. Click “Use this Address” to continue, and be redirected to a Review-Order page.
+                    Click on “Use this Address” to make your shipping & billing information identical, or enter a different billing address with the provided fields. Click “Use this Address” to continue, and you will be redirected to a Review-Order page.
                 </li>
                 <li>
                     Your order review will present your shipping information, payment method, and optional 	promotional codes.
                 </li>
                 <li>
-                To the right of your order reviewing, you will find a “Place your order” link. If you want to finalize this order of socks, click on “Place your order.” You will then be redirected to a new page with a confirmation number, and receive a confirmation email to your registered account 	email address. 
+                To the right of your order reviewing you will find a “Place your order” link. If you want to finalize this order of socks, click on “Place your order.” You will then be redirected to a new page with a confirmation number, and receive a confirmation email to your registered account 	email address. 
                 </li>
             </ol>  
         </div>       
